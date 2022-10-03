48e1262d708cddd54d48514ae7a7b2eb84365155
Víctor Arranz Barcenilla <98172069+vicarbar@users.noreply.github.com>
2022-10-03 17:11:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import os
 #from dotenv import load_dotenv
 #from matplotlib.cbook import print_cycles # In order to get the api key from the .env file
 #load_dotenv()
+#pip uninstall telebot
+#pip uninstall pytelegrambotapi
 import telebot
-from telebot import types
+#from telebot import types
+from telebot.types import *
 from emojiflags.lookup import lookup
 from datetime import datetime
 from bs4 import BeautifulSoup as bs
